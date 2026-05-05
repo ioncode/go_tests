0d9f28003779b25150217559dd3ee4f7245be7fc
Andrey Ponteleev <ionrussia@gmail.com>
2026-05-05 14:43:01 +0300

    testify assert for abs func

--- abs/main_test.go
+++ abs/main_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestMain(t *testing.T) {
 	tests := []struct {
@@ -33,6 +37,9 @@ func TestMain(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			//using testify/assert
+			assert.Equal(t, test.expected, Abs(test.value))
+			// native testing
 			if result := Abs(test.value); result != test.expected {
 				t.Errorf("For value %v expected result %v not equal with actual %v", test.value, test.expected, result)
 			}
